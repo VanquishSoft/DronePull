fac86fb74f3f751eaadd41d78da603e9d466e068
VanquishSoft <60197142+VanquishSoft@users.noreply.github.com>
2022-12-19 06:27:31 -0700

    Oled fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs').promises;
 
 const usbDetect = require('usb-detection');
 const drivelist = require('drivelist');
+var font = require('oled-font-5x7');
 var i2c = require('i2c-bus'),
   i2cBus = i2c.openSync(1),
   oled = require('oled-i2c-bus');
@@ -9,25 +10,29 @@ var i2c = require('i2c-bus'),
 var opts = {
     width: 128,
     height: 32,
-    address: 0x3D
+    address: 0x3C
 };
 
 var oled = new oled(i2cBus, opts);
 oled.turnOnDisplay();
+oled.clearDisplay();
 let usbPath = "";
 
 usbDetect.startMonitoring();
 
 // Detect insert
 usbDetect.on('add', () => {
-    drivelist.list().then((drives) => {
-        drives.forEach((drive) => {
-            if (drive.isUSB) {
-                usbPath = drive.mountpoints[0].path;
-                oled.fillRect(1, 1, 10, 20, 1);
-            }
+    const poll = setInterval(() => {
+        drivelist.list().then((drives) => {
+            drives.forEach((drive) => {
+                if (drive.isUSB && usbPath == "") {
+                    const usbPath = drive.mountpoints[0].path;
+                    oled.setCursor(1, 1);
+                    oled.writeString(font, 1, 'Device Connected', 1, true);
+                }
+            })
         })
-    })
+    }, 2000)
 });
 
 const main = async () =>
